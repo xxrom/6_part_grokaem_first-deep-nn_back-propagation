01b7d93cc6c704015f9db64901685d036f6ec9f1
Chernyshov_Nikita <xpom5566@mail.ru>
2020-05-09 04:12:30 +0300

    feat: learning process for all learning data (for streetLight)! =)

--- 0_lighter_learning.py
+++ 0_lighter_learning.py
@@ -12,13 +12,22 @@ walk_vs_stop = np.array([0, 1, 0, 1, 1, 0])
 input = streetlights[0]
 goal_prediction = walk_vs_stop[0]
 
-for i in range(20):
-  prediction = input.dot(weights)
+for i in range(40):
+  error_for_all_lights = 0
 
-  error = (goal_prediction - prediction)**2
-  delta = prediction - goal_prediction
-  # Delta vector!
-  weights -= alpha * (input * delta)
+  for item in range(len(walk_vs_stop)):
+    input = streetlights[item]
+    goal_prediction = walk_vs_stop[item]
 
-  print('error^2: %.5f, delta: %.5f, delta-weight:' % (error, delta),
-        alpha * (input * delta))
+    prediction = input.dot(weights)
+
+    error = (goal_prediction - prediction)**2
+    delta = prediction - goal_prediction
+    # Delta vector!
+    weights -= alpha * (input * delta)
+
+    print('error^2: %.5f, delta: %.5f, delta-weight:' % (error, delta),
+          alpha * (input * delta))
+    error_for_all_lights += error
+
+  print('global_sum_error^2 = %.5f' % error_for_all_lights)
